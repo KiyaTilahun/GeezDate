f27aeab448bf1f67223ffff237d1408c93bed345
kiyatilahun <kiyatilahun0@gmail.com>
2024-12-25 15:02:40 +0300

    has updated the date

--- src/GeezDate.php
+++ src/GeezDate.php
@@ -2,6 +2,8 @@
 
 namespace GeezDate\GeezDate;
 
+use phpDocumentor\Reflection\DocBlock\Tags\Throws;
+
 class GeezDate
 {
 
@@ -12,58 +14,52 @@ class GeezDate
         $jsonFile = realpath(__DIR__ . '/../resources/jsons/geez.json');
         $this->geezNumbers = json_decode(file_get_contents($jsonFile), true);
     }
-    public  function hello($number)
-    {
 
+    public function changeDatetoGeez($date)
+{
+
+    $dateParts = explode('-', $date);
+
+    if (count($dateParts) != 3) {
+        throw new \Exception("Invalid date format. Please use YYYY-MM-DD.");
+    }
+
+    $year = ltrim($dateParts[0], '0');
+    $month = ltrim($dateParts[1], '0');
+    $day = ltrim($dateParts[2], '0');
+    if ($month < 1 || $month > 13) {
+        throw new \Exception("Invalid month. Please use a month between 1 and 12.");
+    }
+
+    if ($day < 1 || $day > 31) {
+        throw new \Exception("Invalid day. Please use a valid day for the given month.");
+    }
+
+    $geezYear = $this->changeNumber($year);
+    $geezMonth = $this->changeNumber($month);
+    $geezDay = $this->changeNumber($day);
+
+    return $geezYear . "-" . $geezMonth . "-" . $geezDay;
+}
+    public  function changeNumber(string $number)
+    {
+        $number = (int)$number;
         $geezNumber = '';
         if ($number <= 1000) {
             $number = strval($number);
             $geezNumber = $this->geezNumbers['numbers'][$number];
             return  $geezNumber;
         } else if ($number > 1000 && $number < 10000) {
+
             return $this->hundredMultiplier($number);
+
         } else if ($number >= 10000 && $number <= 100000) {
             return $this->thousandMultiplier($number);
-        } else if ($number > 100000) {
-            $result = '';
-
-            $tenthnumber = intdiv($number, 10000);
-            $thousandth = $number % 10000;
-            $backnumber = $this->hundredMultiplier($thousandth);
-            if ($tenthnumber < 10000) {
-                $frontnumber = $this->hundredMultiplier($tenthnumber);
-                return $frontnumber . $this->geezNumbers['multiplier']["1000"] . $backnumber;
-            } else {
-
-                if($tenthnumber>10000 && $tenthnumber<100000 ) {
-                    $frontnumber=$this->thousandMultiplier($tenthnumber);
-                    return $frontnumber . $this->geezNumbers['multiplier']["1000"] . $backnumber;
-                     }
-                else  {
-
-                    $backnumber = [];
-                    while ($tenthnumber > 100000) {
-
-                        $thousandth = $tenthnumber % 10000;
-                        if($tenthnumber>10000 && $tenthnumber<100000 ) {
-                            $newfront=$this->thousandMultiplier($tenthnumber);
-                            $frontnumber =$newfront . $this->geezNumbers['multiplier']["1000"] . $this->hundredMultiplier($thousandth);
-                            return $frontnumber . $this->geezNumbers['multiplier']["1000"] . $backnumber;
-
-                         }
-                         else{
-                            $backnumber[]= $this->hundredMultiplier($thousandth);
-                            $tenthnumber=intdiv($tenthnumber, 10000);
-                            // dd($tenthnumber);
-                         }
-
-
-
-                    }
-                }
-
-            }
         }
+            else{
+                return  throw new \Exception("Please use a number from 1-100,000");;
+            }
+
     }
 
 
--- tests/MyTest.php
+++ tests/MyTest.php
@@ -2,7 +2,14 @@
 
 use GeezDate\GeezDate\Facades\GeezDate;
 
-it('can test', function () {
-   dd(GeezDate::hello(934567891));
-    expect(true)->toBeTrue();
+it('converts a valid date to Geez format', function () {
+
+
+    $date = '2024-13-25';
+    $expectedResult = GeezDate::changeNumber('2024') . ' ' . GeezDate::changeNumber('12') . ' ' . GeezDate::changeNumber('25');
+
+    $geezDate = GeezDate::changeDatetoGeez($date);
+    dd($expectedResult);
+
+    expect($geezDate)->toBe($expectedResult);
 });
