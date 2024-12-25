2927d9950c4161b8f52b34f6e433b2cfe199c7a9
KiyaTilahun <KiyaTilahun@users.noreply.github.com>
2024-12-25 12:38:20 +0000

    Fix styling

--- src/GeezDate.php
+++ src/GeezDate.php
@@ -2,8 +2,6 @@
 
 namespace GeezDate\GeezDate;
 
-use phpDocumentor\Reflection\DocBlock\Tags\Throws;
-
 class GeezDate
 {
     public $geezNumbers;
@@ -15,49 +13,50 @@ class GeezDate
     }
 
     public function changeDatetoGeez($date)
-{
-
-    $dateParts = explode('-', $date);
-
-    if (count($dateParts) != 3) {
-        throw new \Exception("Invalid date format. Please use YYYY-MM-DD.");
-    }
-
-    $year = ltrim($dateParts[0], '0');
-    $month = ltrim($dateParts[1], '0');
-    $day = ltrim($dateParts[2], '0');
-    if ($month < 1 || $month > 13) {
-        throw new \Exception("Invalid month. Please use a month between 1 and 12.");
-    }
-
-    if ($day < 1 || $day > 31) {
-        throw new \Exception("Invalid day. Please use a valid day for the given month.");
-    }
-
-    $geezYear = $this->changeNumber($year);
-    $geezMonth = $this->changeNumber($month);
-    $geezDay = $this->changeNumber($day);
-
-    return $geezYear . "-" . $geezMonth . "-" . $geezDay;
-}
-    public  function changeNumber(string $number)
     {
-        $number = (int)$number;
+
+        $dateParts = explode('-', $date);
+
+        if (count($dateParts) != 3) {
+            throw new \Exception('Invalid date format. Please use YYYY-MM-DD.');
+        }
+
+        $year = ltrim($dateParts[0], '0');
+        $month = ltrim($dateParts[1], '0');
+        $day = ltrim($dateParts[2], '0');
+        if ($month < 1 || $month > 13) {
+            throw new \Exception('Invalid month. Please use a month between 1 and 12.');
+        }
+
+        if ($day < 1 || $day > 31) {
+            throw new \Exception('Invalid day. Please use a valid day for the given month.');
+        }
+
+        $geezYear = $this->changeNumber($year);
+        $geezMonth = $this->changeNumber($month);
+        $geezDay = $this->changeNumber($day);
+
+        return $geezYear.'-'.$geezMonth.'-'.$geezDay;
+    }
+
+    public function changeNumber(string $number)
+    {
+        $number = (int) $number;
         $geezNumber = '';
         if ($number <= 1000) {
             $number = strval($number);
             $geezNumber = $this->geezNumbers['numbers'][$number];
-            return  $geezNumber;
-        } else if ($number > 1000 && $number < 10000) {
+
+            return $geezNumber;
+        } elseif ($number > 1000 && $number < 10000) {
 
             return $this->hundredMultiplier($number);
 
-        } else if ($number >= 10000 && $number <= 100000) {
+        } elseif ($number >= 10000 && $number <= 100000) {
             return $this->thousandMultiplier($number);
+        } else {
+            return throw new \Exception('Please use a number from 1-100,000');
         }
-            else{
-                return  throw new \Exception("Please use a number from 1-100,000");;
-            }
 
     }
 
--- tests/MyTest.php
+++ tests/MyTest.php
@@ -4,9 +4,8 @@ use GeezDate\GeezDate\Facades\GeezDate;
 
 it('converts a valid date to Geez format', function () {
 
-
     $date = '2024-12-25';
-    $expectedResult = GeezDate::changeNumber(2024) . '-' . GeezDate::changeNumber(12) . '-' . GeezDate::changeNumber(25);
+    $expectedResult = GeezDate::changeNumber(2024).'-'.GeezDate::changeNumber(12).'-'.GeezDate::changeNumber(25);
 
     $geezDate = GeezDate::changeDatetoGeez($date);
 
